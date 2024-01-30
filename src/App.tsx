caa97289934c86ee820e8372a4f92ba047d63c02
doducchieni86 <doducchieni86@gmail.com>
2024-01-30 15:32:14 +0700

    base

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='text-center text-red-300 text-3xl font-medium'>Project Typescript</h2>
+    </div>
+  );
+}
+
+export default App;
